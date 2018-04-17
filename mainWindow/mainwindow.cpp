5f965fedaca76def147c4a8ac116260bd36d0b2e
Peter Mackenzie-Helnwein <mackheln@gmail.com>
2018-04-17 14:14:57 -0700

    cleaning up mainwindow.cpp

--- mainWindow/mainwindow.cpp
+++ mainWindow/mainwindow.cpp
@@ -29,42 +29,10 @@
 #include <QJsonValue>
 
 // OpenSees include files
-#include <Node.h>
-#include <ID.h>
-#include <SP_Constraint.h>
-#include <MP_Constraint.h>
-#include <Domain.h>
 #include <StandardStream.h>
-#include <LinearCrdTransf3d.h>
-#include <DispBeamColumn3d.h>
-#include <PySimple1.h>
-#include <TzSimple1.h>
-#include <QzSimple1.h>
-#include <ZeroLength.h>
-#include <LegendreBeamIntegration.h>
-#include <ElasticSection3d.h>
-#include <LinearSeries.h>
-#include <NodalLoad.h>
-#include <LoadPattern.h>
-#include <SimulationInformation.h>
-
-#include <LoadControl.h>
-#include <RCM.h>
-#include <PlainNumberer.h>
-#include <NewtonRaphson.h>
-#include <CTestNormDispIncr.h>
-#include <TransformationConstraintHandler.h>
-#include <PenaltyConstraintHandler.h>
-#include <BandGenLinSOE.h>
-#include <BandGenLinLapackSolver.h>
-#include <StaticAnalysis.h>
-#include <AnalysisModel.h>
-
-#include <soilmat.h>
 
 StandardStream sserr;
 OPS_Stream *opserrPtr = &sserr;
-//Domain theDomain;
 
 //SimulationInformation simulationInfo;
 
